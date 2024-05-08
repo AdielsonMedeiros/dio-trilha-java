4a786902487081bb11733e57dd43d15d977a8951
Adielson Medeiros <medeirosadielson@gmail.com>
2024-05-08 12:48:51 -0300

    desafio iphone

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -12,16 +12,16 @@ public class Contador {
 			//chamando o método contendo a lógica de contagem
 			contar(parametroUm, parametroDois);
 		
-		}catch (ParametrosInvalidosException exception) {
+		}catch (SimulacaoBancaria exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
 			System.out.println(exception.getMessage());
 		}
 		
 	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+	static void contar(int parametroUm, int parametroDois ) throws SimulacaoBancaria {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		if(parametroUm > parametroDois) {
-			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+			throw new SimulacaoBancaria("O segundo parâmetro deve ser maior que o primeiro");
 		}
 		
 		int contagem = parametroDois - parametroUm;
